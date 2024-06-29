9c2e1b7114e365ec0fa6237db1c7e66a8c2689c0
ridheshcybe <80441770+ridheshcybe@users.noreply.github.com>
2024-06-29 19:47:15 +0545

    added health and graceful shutdown

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ const express = require("express");
 
 // run the websocket server and the http server
 const app = require('express-ws')(express());
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './index.html'));
+});
+
+app.get('/health', (req, res) => res.status(200).send(process.uptime()));
+
+const server = app.listen(port)
+
+process.on('SIGTERM', () => {
+    console.warn('SIGTERM signal received: closing HTTP server')
+    server.close(() => {
+        console.warn('HTTP server closed')
+    })
 })
